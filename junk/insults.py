4e2a0469186f746fc279c75fe620e44542ffca3d
Max Shinnerl <maxshinnerl@gmail.com>
2021-11-02 23:18:58 -0700

    more insults

--- junk/insults.py
+++ junk/insults.py
@@ -8,7 +8,7 @@ def get_insults(sender, receiver):
         receiver + ", " + sender + " thinks you are cringe and NOT based",
         receiver + ", your teamshots are off bro",
         receiver + ", you look like you prefer pancakes (very WeirdChamp)",
-        receiver + ", you're cute." + sender + ", be nicer you twat",
+        receiver + ", you're cute. " + sender + ", be nicer you twat",
         receiver + ", you look like you unironically use shatterdive",
         receiver + ", you look like you main warlock",
         receiver + ", you look like you main hunter",
@@ -16,8 +16,18 @@ def get_insults(sender, receiver):
         receiver + ", is your favorite element lead?",
         receiver + ", " + sender + " thinks you're cute but doesn't know how to say it just yet",
         receiver + ", sosig",
-        receiver + ", you do strikes for fun"
-
+        receiver + ", you do strikes for fun",
+        receiver + ", " + sender + " ordered you a fresh avocado milkshake (code MARCUS at Homi Japanese Ramen)",
+        receiver + ", what do you think of Tottenham?\n\n\n\n\n\n SHIIIIIIIIIIIIIIT",
+        receiver + ", you're dumb",
+        receiver + ", you smell",
+        receiver + ", no YOU'RE dumb",
+        receiver + ", if you and " + sender + " were hanging off a cliff, I'd save...myself the bother and go to Burger King",
+        receiver + ", you are looking NOT hydrated.  CRINGE.  Go drink water.",
+        receiver + ", gangly mf",
+        receiver + ", brig main OMEGALUL",
+        receiver + ", you look like you unironically enjoy Renaissance Faire's (very cringe)",
+        receiver + ", haha stinky"
     ]
 
     return insults
