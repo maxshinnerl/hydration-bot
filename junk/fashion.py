df963fae9050c1bd75ef482af5eda3a16945930a
maxshinnerl <maxshinnerl@gmail.com>
2022-09-29 12:10:38 -0700

    new fashion responses

--- junk/fashion.py
+++ junk/fashion.py
@@ -7,7 +7,24 @@ def get_fashion():
 
     fashion = [
             "bro delete this",
-            "bouta make me act up SHEEEEESH"
+            "bouta make me act up SHEEEEESH",
+            "0/10, try harder",
+            "1/10",
+            "2/10",
+            "3/10",
+            "4/10",
+            "5/10",
+            "5/6",
+            "6/10",
+            "7/10",
+            "8/10",
+            "9/10",
+            "10/10",
+            "wow, cool and original shader choice :D",
+            "https://tenor.com/view/ew-yuck-gif-13390919",
+            "https://tenor.com/view/wow-oh-my-god-omg-shocked-surprised-gif-15089848",
+            "https://tenor.com/view/throw-up-dry-heave-vomit-gross-eww-gif-23254765",
+            "https://tenor.com/view/lebron-james-los-angeles-lakers-look-up-what-is-that-nba-gif-16045196"
     ]
 
 
