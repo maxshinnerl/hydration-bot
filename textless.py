a995c26ae7743e590b2acdc7c209af96b18e49da
maxshinnerl <maxshinnerl@gmail.com>
2023-01-23 11:29:51 -0800

    channel name bugfix

--- textless.py
+++ textless.py
@@ -9,7 +9,7 @@ def process(message):
     print("PROCESSING", flush=True)
     response = None
 
-    if str(message.channel).isin(["fashion", "purgatory"]):
+    if str(message.channel) in ["fashion", "purgatory"]:
         fashion_response = fashion.get_fashion()
         response = random.choice(fashion_response)
 
